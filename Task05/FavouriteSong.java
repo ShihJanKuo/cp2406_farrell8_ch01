e850023edea176b59198d5ea0f49dedb0f803847
ShihJanKuo <shihjan.kuo@my.jcu.edu.au>
2021-06-13 21:20:54 +1000

    Task

---
+++ Task05/FavouriteSong.java
@@ -0,0 +1,9 @@
+public class FavouriteSong
+{
+    public static void main(String[] args)
+    {
+        String lyrics = "Smells Like Teen Spirit \nBohemian Rhapsody\n" +
+                "Like A Rolling Stone\nI Can't Get No Satisfaction";
+        System.out.print(lyrics);
+    }
+}
---
+++ Task05/MovieQuote.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Help me, Obi-Wan Kenobi. You’re my only hope.");
+        System.out.println("Leia Organa");
+        System.out.println("Star Wars: Episode IV - A New Hope");
+        System.out.println("1977");
+
+    }
+}
