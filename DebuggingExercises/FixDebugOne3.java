78db203e7b9f00d964cb229bdba4ce05653416b1
ShihJanKuo <shihjan.kuo@my.jcu.edu.au>
2021-06-13 20:57:44 +1000

    Debugging Exercises

---
+++ DebuggingExercises/FixDebugOne3.java
@@ -0,0 +1,10 @@
+public class FixDebugOne3
+{
+   /* This program displays some output */
+   public static void main(String[] args)
+   {
+      System.out.println("Over the river");
+      system.out.println("and through the woods");
+      SysTem.out.println("to Grandmother's house we go");
+   }
+}
